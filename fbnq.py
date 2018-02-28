bd9dacf29022a2421f391708070c588d9ebce546
guojing <guojing_zb@cmos.chinamobile.com>
2018-02-28 17:08:08 +0800

    This is my Python excise.

---
+++ fbnq.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''
+# Fibonacci series: 斐波纳契数列
+#两个元素的和 确定下一个数
+
+a,b=0,1
+while b < 10000:
+	print(b , end=',')
+	a,b = b,a+b
+'''
+'''
+flag = 1
+while (flag):
+	print ('欢迎访问菜鸟教程!')
+	
+print ("Good bye!")
+'''
+
+'''
+sites = ["Baidu", "Google","Runoob","Taobao"]
+for sit in sites:
+    if sit == "Runoob":
+        print("菜鸟教程!")
+        break
+    print("循环数据 " + sit)
+else:
+    print("没有循环数据!")
+print("完成循环!")
+'''
+
+'''
+#pass
+for letter in 'Runoob': 
+   if letter == 'o':
+      pass
+      print ('执行 pass 块')
+   print ('当前字母 :', letter)
+ 
+print ("Good bye!")
+'''
+
+
